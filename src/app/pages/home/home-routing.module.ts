19ed530f0ff9333e103f9536543f3b42fff25e79
Vane <primestone\vanessa.sora.ext>
2020-04-02 15:52:12 -0500

    implement lazyload

---
+++ src/app/pages/home/home-routing.module.ts
@@ -0,0 +1,76 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home.component';
+import { FindCourierComponent } from '../find-courier/find-courier.component';
+import { FindMedicineComponent } from '../find-medicine/find-medicine.component';
+import { FindPatientComponent } from '../find-patient/find-patient.component';
+import { FindUserComponent } from '../find-user/find-user.component';
+import { SchedulingComponent } from '../scheduling/scheduling.component';
+import { StatusChangeComponent } from '../status-change/status-change.component';
+import { StubsListComponent } from '../stubs-list/stubs-list.component';
+import { NewCourierComponent } from '../new-courier/new-courier.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'find-courier',
+        pathMatch: 'full'
+      },
+      {
+        path: 'find-courier',
+        component: FindCourierComponent
+      },
+      {
+        path: 'find-medicine',
+        component: FindMedicineComponent
+      },
+      {
+        path: 'find-patient',
+        component: FindPatientComponent
+      },
+      {
+        path: 'find-user',
+        component: FindUserComponent
+      },
+      {
+        path: 'new-courier',
+        component: NewCourierComponent,
+      },
+      {
+        path: 'new-medicine',
+        component: FindMedicineComponent
+      },
+      {
+        path: 'new-patient',
+        component: FindPatientComponent
+      },
+      {
+        path: 'new-user',
+        component: FindUserComponent
+      },
+      {
+        path: 'scheduling',
+        component: SchedulingComponent
+      },
+      {
+        path: 'status-change',
+        component: StatusChangeComponent
+      },
+      {
+        path: 'stubs-list',
+        component: StubsListComponent
+      },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { FindCourierComponent } from '../find-courier/find-courier.component';
+import { FindMedicineComponent } from '../find-medicine/find-medicine.component';
+import { FindPatientComponent } from '../find-patient/find-patient.component';
+import { FindUserComponent } from '../find-user/find-user.component';
+import { NewCourierComponent } from '../new-courier/new-courier.component';
+import { NewMedicineComponent } from '../new-medicine/new-medicine.component';
+import { NewPatientComponent } from '../new-patient/new-patient.component';
+import { NewUserComponent } from '../new-user/new-user.component';
+import { SchedulingComponent } from '../scheduling/scheduling.component';
+import { StatusChangeComponent } from '../status-change/status-change.component';
+import { StubsListComponent } from '../stubs-list/stubs-list.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    FindCourierComponent,
+    FindMedicineComponent,
+    FindPatientComponent,
+    FindUserComponent,
+    NewCourierComponent,
+    NewMedicineComponent,
+    NewPatientComponent,
+    NewUserComponent,
+    SchedulingComponent,
+    StatusChangeComponent,
+    StubsListComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+  ]
+})
+export class HomeModule { }
