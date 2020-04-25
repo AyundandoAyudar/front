b239fb5624738b8322113e251df2a90a615cc5ed
JuanSe Dussan <Juan.Dussan@nordstrom.com>
2020-04-24 20:06:06 -0500

    made aleatories mocks

--- __mocks__/models/mockListOrders.ts
+++ __mocks__/models/mockListOrders.ts
@@ -1,3 +1,4 @@
 import { mockOrders } from './mockOrders';
 
-export const mockListOrders = () => new Array(30).fill(mockOrders());
+export const mockListOrders = () =>
+  new Array(30).fill(0).map(() => mockOrders());
--- __mocks__/models/mockOrders.ts
+++ __mocks__/models/mockOrders.ts
@@ -1,8 +1,8 @@
 import { Order } from '../../src/app/shared/models/order.model';
 import * as faker from 'faker';
 
-export const mockOrders = (
-  props: Partial<Order> = {
+export const mockOrders = (props?: Partial<Order>) =>
+  new Order({
     id: `no-valid-order-id-123-asd-${faker.random.number()}`,
     orderId: faker.random.number(),
     patientIdNumber: `no-valid-patientIdNumber-123-asd-${faker.random.number()}`,
@@ -14,5 +14,5 @@ export const mockOrders = (
     deliveredDate: faker.date.past(),
     canceledDate: faker.date.past(),
     annotations: faker.lorem.sentences(),
-  }
-) => new Order(props);
+    ...props,
+  });
