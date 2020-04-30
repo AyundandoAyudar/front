c2a5e1e3cb32862277340c6dcc4cc587b8c987b0
JuanSe Dussan <Juan.Dussan@nordstrom.com>
2020-04-29 19:23:37 -0500

    feat(#48): Lock schedule if previuous orders are still open
    Cada mañana se debe dar una alerta para el cierre (status "cerrada" o "fallida") de las ordenes que estén abiertas, si hay ordenes abiertas no se pueden generar nuevas ordenes
    
    Closes #48

--- src/app/shared/services/orders.service.ts
+++ src/app/shared/services/orders.service.ts
@@ -7,21 +7,35 @@ import {
 } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Order } from '../models/order.model';
+import * as moment from 'moment';
+
+import { Order, OrderStatus } from '../models/order.model';
 import { Deserializable } from '../models/deserializable.model';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrdersService {
-  constructor(private firestore: AngularFirestore) {}
+  constructor(
+    private firestore: AngularFirestore,
+    public snackBar: MatSnackBar
+  ) {
+    this.getActiveOrders().subscribe(this.checkOrdersOpen);
+  }
 
   // ------------------------------ Orders ----------------------------------
 
   ordersCollection: AngularFirestoreCollection<Order>;
   orders: Observable<Order[]>;
+  ordersAreStillOpen = false;
 
   createOrder(order: Order): Promise<DocumentReference> {
+    if (this.ordersAreStillOpen) {
+      return Promise.reject(
+        'Tienes órdenes pendientes por cerrar, por favor revisalas'
+      );
+    }
     return this.firestore.collection('orders').add({ ...order });
   }
 
@@ -58,7 +72,42 @@ export class OrdersService {
       );
   }
 
+  getActiveOrders() {
+    const todayZeroHrs = moment(moment().format('L'));
+    return this.getOrders((ref) =>
+      ref
+        .where('status', '==', OrderStatus.Open)
+        .orderBy('scheduleDate')
+        .endAt(todayZeroHrs.format())
+    ).pipe(
+      // ? We can`t do multiple range filters per query :(
+      // ? So I add front filter to do it
+      map((orders) =>
+        orders
+          .filter(
+            (order) =>
+              !(
+                order.deleted_at &&
+                moment(order.deleted_at).isBefore(todayZeroHrs)
+              )
+          )
+          .filter(
+            (order) =>
+              !(
+                order.canceledDate &&
+                moment(order.canceledDate).isBefore(todayZeroHrs)
+              )
+          )
+      )
+    );
+  }
+
   updateOrder(order: Order) {
+    if (this.ordersAreStillOpen) {
+      return Promise.reject(
+        'Tienes órdenes pendientes por cerrar, por favor revisalas'
+      );
+    }
     Deserializable.cleanNull(order);
     return this.firestore.doc('orders/' + order.id).update({ ...order });
   }
@@ -67,6 +116,18 @@ export class OrdersService {
     let order = new Order({ id: orderId, deleted_at: new Date() });
     return this.updateOrder(order);
   }
+
+  checkOrdersOpen = (orders: Order[]) => {
+    if (orders.length) {
+      // 1 o more orders pendent
+      this.ordersAreStillOpen = true;
+      this.snackBar.open(
+        'Tienes órdenes pendientes por cerrar, por favor revisalas',
+        'X',
+        {}
+      );
+    }
+  };
 
   // --------------------------------------------------------------------------
 }
